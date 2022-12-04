ee741c9233bdcd88fa42ba7d1490d002a2b6cb4d
Hermes <hermes.kuubo@gmail.com>
2022-12-03 21:13:48 -0300

    começo do sistema de dificuldade de level

--- Assets/Scripts/GeradorInimigo.cs
+++ Assets/Scripts/GeradorInimigo.cs
@@ -7,7 +7,7 @@ public class GeradorInimigo : MonoBehaviour
     [SerializeField] private GameObject[] inimigos;
 
     private int pontos = 0;
-    private int level = 1;
+    [SerializeField]private int level = 1;
 
     private float esperaInimigo = 0f;
     [SerializeField] private float tempoEspera = 5f;
@@ -34,9 +34,25 @@ public class GeradorInimigo : MonoBehaviour
 
         if (esperaInimigo <= 0f)
         {
+
+            GameObject inimigoCriado;
+
+            //Deciddindo qual inimigo vai ser cirado com base no level
+            float chance = Random.Range(0, level);
+
+            if (chance > 2)
+            {
+                inimigoCriado = inimigos[1];
+            }
+            else
+            {
+                inimigoCriado = inimigos[0];
+            }
+
+
             //Criando um inimigo
             Vector3 posicao = new Vector3(Random.Range(-8f, 8f), Random.Range(6f, 16f), 0f);
-            Instantiate(inimigos[0], posicao, transform.rotation);
+            Instantiate(inimigoCriado, posicao, transform.rotation);
 
 
             esperaInimigo = tempoEspera;
